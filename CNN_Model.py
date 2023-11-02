4ceb589073d30e53104a5bf0264c99eb2be2a6f2
nbivens <nicholas.bivens@geotoolkit.net>
2023-11-02 03:03:22 -0500

    changed soft attention

--- CNN_Model.py
+++ CNN_Model.py
@@ -9,64 +9,53 @@ from keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
 
 
-# Global View (U-Net)
-def create_global_view():
+def create_global_view(initial_layer = 8, dropout = 0.2):
     inputs = layers.Input(shape=(None, 3))  # Assuming the logs are 1D sequences
     
-    # Adding a normalization layer
-    x = Normalization(axis=-1)(inputs) 
+    x = layers.Masking(mask_value=-99)(inputs)  # Using -99 as the mask value
 
     # Encoding layer 1
-    # HYPERPARAMETER: Adjust the number of filters (64) and kernel size (3)
-    conv1 = layers.Conv1D(64, 3, activation="relu", padding="same")(x)
+    conv1 = layers.Conv1D(initial_layer, 3, activation="relu", padding="same")(x)
     conv1 = layers.BatchNormalization()(conv1)
-    # HYPERPARAMETER: Adjust dropout rate (0.5)
-    conv1 = layers.Dropout(0.5)(conv1)
+    conv1 = layers.Dropout(dropout)(conv1)
     pool1 = layers.MaxPooling1D()(conv1)
 
-    # Encoding layer 2 (and so on, if you want deeper architecture)
-    # HYPERPARAMETER: Adjust the number of filters (64) and kernel size (3)
-    conv2 = layers.Conv1D(128, 3, activation="relu", padding="same")(pool1)
+    # Encoding layer 2
+    conv2 = layers.Conv1D(2*initial_layer, 3, activation="relu", padding="same")(pool1)
     conv2 = layers.BatchNormalization()(conv2)
-    # HYPERPARAMETER: Adjust dropout rate (0.5)
-    conv2 = layers.Dropout(0.5)(conv2)
+    conv2 = layers.Dropout(dropout)(conv2)
     pool2 = layers.MaxPooling1D()(conv2)
 
-    # Middle layer (You can continue with more encoding layers before this)
-    # HYPERPARAMETER: Adjust the number of filters (64) and kernel size (3)
-    conv_middle = layers.Conv1D(256, 3, activation="relu", padding="same")(pool2)
+    # Middle layer
+    conv_middle = layers.Conv1D(4*initial_layer, 3, activation="relu", padding="same")(pool2)
     conv_middle = layers.BatchNormalization()(conv_middle)
-    # HYPERPARAMETER: Adjust dropout rate (0.5)
-    conv_middle = layers.Dropout(0.5)(conv_middle)
+    
+    conv_middle = layers.Dropout(dropout)(conv_middle)
 
     # Decoding layer 1
     up1 = layers.UpSampling1D()(conv_middle)
     
-    # Ensure sizes match before concatenating
-    # cropped_conv2 = layers.Cropping1D(cropping=((0, 1)))(conv2)  # Adjust as needed
+    # # Ensure sizes match before concatenating
+    # cropped_conv2 = layers.Cropping1D(cropping=((1,0)))(conv2)
     merge1 = layers.concatenate([conv2, up1])
     
-    
-    # HYPERPARAMETER: Adjust the number of filters (64) and kernel size (3)
-    decode1 = layers.Conv1D(128, 3, activation="relu", padding="same")(merge1)
+    decode1 = layers.Conv1D(2*initial_layer, 3, activation="relu", padding="same")(merge1)
     decode1 = layers.BatchNormalization()(decode1)
-    # HYPERPARAMETER: Adjust dropout rate (0.5)
-    decode1 = layers.Dropout(0.5)(decode1)
+    decode1 = layers.Dropout(dropout)(decode1)
 
-    # Decoding layer 2 (and so on, mirroring the encoding architecture)
+    # Decoding layer 2
     up2 = layers.UpSampling1D()(decode1)
     
     # Ensure sizes match before concatenating for the second merge
-    # cropped_conv1 = layers.Cropping1D(cropping=((0, 1)))(conv1)  # Adjust as needed
+    # cropped_conv1 = layers.Cropping1D(cropping=((2,0)))(conv1)  # Adjusted cropping
     merge2 = layers.concatenate([conv1, up2])
 
-    # HYPERPARAMETER: Adjust the number of filters (64) and kernel size (3)
-    decode2 = layers.Conv1D(64, 3, activation="relu", padding="same")(merge2)
+    decode2 = layers.Conv1D(initial_layer, 3, activation="relu", padding="same")(merge2)
     decode2 = layers.BatchNormalization()(decode2)
-    # HYPERPARAMETER: Adjust dropout rate (0.5)
-    decode2 = layers.Dropout(0.5)(decode2)
+    decode2 = layers.Dropout(dropout)(decode2)
 
     return models.Model(inputs, decode2)
+
 
 # Local View
 def create_local_view():
@@ -91,59 +80,120 @@ for file in files:
 
 train_dfs, val_dfs = train_test_split(dfs, test_size=0.2, random_state=42) # Here 20% of the data is kept for validation
 
+# Normalize function
+def normalize_data(data, mean, std):
+    return (data - mean) / std
 
-def ensure_even_length(sequence):
-    # If the sequence length is odd, append zeros to make it even
+
+def ensure_even_length(sequence, padding_value=-99):
+    # If the sequence length is odd, append padding to make it even
     if sequence.shape[0] % 2 != 0:
         padding_shape = list(sequence.shape)
         padding_shape[0] = 1  # we only need to add one row
         
         # Create a padding with the same number of columns as the sequence
-        padding = np.zeros(padding_shape)
+        padding = np.full(padding_shape, padding_value)
         
         # Append padding to the sequence
         sequence = np.vstack([sequence, padding])
         
     return sequence
 
+def pad_to_max_length(sequence, max_length, padding_value=-99):
+    padding = [(0, max_length - sequence.shape[0]), (0, 0)]
+    return np.pad(sequence, padding, mode='constant', constant_values=padding_value)
+
+
+def adjust_max_length(max_length):
+    last_digit = max_length % 10
+    
+    if last_digit in [2, 6]:
+        return max_length + 2
+    elif last_digit == 0:
+        return max_length + 4
+    else:
+        return max_length
+    
+def smooth_labels(y, sigma=1.5):
+    # Ensure y is a 1D array
+    y = y.flatten()
+    smoothed_y = np.zeros_like(y, dtype=float)
+    for idx in np.where(y == 1)[0]:
+        gaussian = np.exp(-0.5 * ((np.arange(len(y)) - idx) / sigma) ** 2)
+        gaussian /= gaussian.sum()
+        smoothed_y += 4* gaussian  # Both are 1D arrays, so this should work
+    return smoothed_y
+
+
+current_dir = os.getcwd()
+# Train your model here
+folder_path = current_dir + "/Data/Formation_DATA/"
+files = [f for f in os.listdir(folder_path) if f.endswith(".csv")]
+
+dfs = []
+for file in files:
+    df = pd.read_csv(folder_path + file)
+    dfs.append(df)
+
+train_dfs, val_dfs = train_test_split(dfs, test_size=0.2, random_state=42) # Here 20% of the data is kept for validation
+
 features = ["GR", "ILD", "DPHI"]
 
+# Compute the mean and standard deviation of the training data
+all_train_data = np.vstack([df[features].values for df in train_dfs])
+mean = np.mean(all_train_data, axis=0)
+std = np.std(all_train_data, axis=0)
 
 # Extract features and labels for training and validation sets
-# Convert to numpy arrays
 X_train = np.array([df[features].values for df in train_dfs])
 y_train = np.array([df["Pick"].values for df in train_dfs])
 
 X_val = np.array([df[features].values for df in val_dfs])
 y_val = np.array([df["Pick"].values for df in val_dfs])
 
+
+# Normalize data
+X_train = [normalize_data(x, mean, std) for x in X_train]
+X_val = [normalize_data(x, mean, std) for x in X_val]
+
+# Ensure even length
 X_train = [ensure_even_length(x) for x in X_train]
 X_val = [ensure_even_length(x) for x in X_val]
 
-# Find the maximum sequence length
-max_length = max(max(len(x) for x in X_train), max(len(x) for x in X_val))
+# Compute max_length after ensuring even lengths
+max_length = max(max(x.shape[0] for x in X_train), max(x.shape[0] for x in X_val))+2
 
-# Pad sequences to have the same length
-X_train = [np.pad(x, ((0, max_length - x.shape[0]), (0, 0)), mode='constant') for x in X_train]
-X_val = [np.pad(x, ((0, max_length - x.shape[0]), (0, 0)), mode='constant') for x in X_val]
+# The UNET structure requires the total size of the input when divided by 4  or divided by 2 be even. 
+max_length = adjust_max_length(max_length)
+
+# Pad to max_length
+X_train = [pad_to_max_length(x, max_length) for x in X_train]
+X_val = [pad_to_max_length(x, max_length) for x in X_val]
+
+# # smooth y values
+y_train = [smooth_labels(y) for y in y_train]
+y_val = [smooth_labels(y) for y in y_val]
+
+# Ensure even length for labels and then pad
+y_train = [ensure_even_length(y.reshape(-1, 1),padding_value=0) for y in y_train]
+y_val = [ensure_even_length(y.reshape(-1, 1),padding_value=0) for y in y_val]
+
+y_train = [pad_to_max_length(y, max_length,padding_value=0) for y in y_train]
+y_val = [pad_to_max_length(y, max_length,padding_value=0) for y in y_val]
 
 
-# Pad label sequences to have the same length as the input sequences
-y_train = [np.pad(y, (0, max_length - len(y)), mode='constant') for y in y_train]
-y_val = [np.pad(y, (0, max_length - len(y)), mode='constant') for y in y_val]
 
 
-def create_model(X_train, y_train, X_val, y_val, learning_rate, epochs):
-    all_data = np.vstack(X_train)
+# def focal_loss(gamma=2., alpha=0.8):
+#     def focal_loss_fixed(y_true, y_pred):
+#         pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+#         pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+#         return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) - K.sum((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
+#     return focal_loss_fixed
 
+def train_model(X_train, y_train, X_val, y_val, learning_rate, epochs):
     global_view = create_global_view()
     local_view = create_local_view()
-
-    # Adapt the normalization layer in global_view
-    global_view.layers[1].adapt(all_data)
-
-    # Adapt the normalization layer in local_view
-    local_view.layers[1].adapt(all_data)
 
     combined = layers.Multiply()([global_view.output, local_view.output])
 
@@ -156,12 +206,22 @@ def create_model(X_train, y_train, X_val, y_val, learning_rate, epochs):
     model = models.Model([global_view.input, local_view.input], output)
     optimizer = Adam(learning_rate=learning_rate)
 
+    # # HYPERPARAMETER: Adjust the optimizer ('adam') and its parameters
+    # model.compile(optimizer=optimizer, loss= focal_loss())
+    
     # HYPERPARAMETER: Adjust the optimizer ('adam') and its parameters
-    model.compile(optimizer=optimizer, loss="binary_crossentropy")
+    model.compile(optimizer=optimizer, loss= tf.losses.BinaryCrossentropy())
 
 
+    # Early stopping parameters
+    patience = 5
+    min_delta = 0.001
+    best_val_loss = float('inf')
+    wait = 0
     train_losses = []
     val_losses = []
+    
+
     # We'll only use this training loop and remove the redundant one.
     for epoch in range(epochs):
         print(f"Epoch {epoch+1}/{epochs}")
@@ -173,7 +233,8 @@ def create_model(X_train, y_train, X_val, y_val, learning_rate, epochs):
             y_train_well = y_train[i].reshape(1, y_train[i].shape[0], 1)
             loss = model.train_on_batch([X_train_well, X_train_well], y_train_well)  # Capturing the loss
             epoch_train_losses.append(loss)
-        train_losses.append(np.mean(epoch_train_losses))
+        mean_train_loss = np.mean(epoch_train_losses)
+        train_losses.append(np.mean(mean_train_loss))
 
         # Validation (optional)
         epoch_val_losses = []
@@ -184,9 +245,17 @@ def create_model(X_train, y_train, X_val, y_val, learning_rate, epochs):
             epoch_val_losses.append(loss)
         mean_val_loss = np.mean(epoch_val_losses)
         val_losses.append(mean_val_loss)
-        print(f"Training Loss: {train_losses[-1]:.4f}, Validation Loss: {mean_val_loss:.4f}")
+        print(f"Training Loss: {train_losses[-1]:.5f}, Validation Loss: {mean_val_loss:.5f}")
 
-
+        # Early stopping check
+        if (best_val_loss - mean_val_loss) > min_delta:
+            best_val_loss = mean_val_loss
+            wait = 0
+        else:
+            wait += 1
+            if wait >= patience:
+                print(f"Early stopping at epoch {epoch+1}. Best validation loss: {best_val_loss:.5f}")
+                break
 
     plt.figure(figsize=(10, 6))
     plt.plot(train_losses, label="Training Loss")
@@ -197,5 +266,74 @@ def create_model(X_train, y_train, X_val, y_val, learning_rate, epochs):
     plt.legend()
     plt.show()
     
+    return model
     
-create_model(X_train, y_train,X_val, y_val, learning_rate = 0.0001,epochs=50)
+    
+model = train_model(X_train, y_train,X_val, y_val, learning_rate = 0.001,epochs=200)
+
+
+current_dir = os.getcwd()
+# Train your model here
+model_path = current_dir + "/Model/"
+
+model.save(model_path)
+
+current_dir = os.getcwd()
+# Train your model here
+model_path = current_dir + "/Model/"
+
+# Load the model
+loaded_model = load_model(model_path, )
+
+
+current_dir = os.getcwd()
+# Train your model here
+folder_path = current_dir + "/Data/Formation_DATA/"
+files = [f for f in os.listdir(folder_path) if f.endswith(".csv")]
+
+test_dfs = []
+for file in files:
+    df = pd.read_csv(folder_path + file)
+    test_dfs.append(df)
+
+features = ["GR", "ILD", "DPHI"]
+
+
+# Extract features and labels for training and validation sets
+X_test = np.array([df[features].values for df in test_dfs])
+y_test = np.array([df["Pick"].values for df in test_dfs])
+
+# Normalize data
+X_test = [normalize_data(x, mean, std) for x in X_test]
+
+# Ensure even length
+X_test = [ensure_even_length(x) for x in X_test]
+
+# Compute max_length after ensuring even lengths
+max_length = max(max(x.shape[0] for x in X_train), max(x.shape[0] for x in X_test))
+max_length = adjust_max_length(max_length)
+
+
+
+# Pad to max_length
+X_test = [pad_to_max_length(x, max_length) for x in X_test]
+
+# # Ensure even length for labels and then pad
+y_test = [smooth_labels(y) for y in y_test]
+y_test = [ensure_even_length(y.reshape(-1, 1),padding_value=0) for y in y_test]
+y_test = [pad_to_max_length(y, max_length,padding_value=0) for y in y_test]
+
+
+folder_path = current_dir + "/Data/Formation_DATA/"
+
+# Ensure the directory exists
+output_dir = os.path.join(current_dir, folder_path, "Predictions")
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# Loop through X_test and y_test
+for df in test_dfs:
+    name=df['SitID'][0]
+    # Save the DataFrame as a CSV file
+    output_path = os.path.join(output_dir, f"predictions_{name}.csv")
+    df.to_csv(output_path, index=False)
